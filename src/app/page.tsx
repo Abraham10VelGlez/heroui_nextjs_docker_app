f22698192891ceddfcf484358e44c817a8374b8c
Abraham10VelGlez <usbrifa@gmail.com>
2025-02-24 14:03:10 -0600

    codeavg2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,50 @@
 import Image from "next/image";
-import Test from "./Test";
+import Backgroudpage from "@/components/Backgroundpage";
+import Skeletonx from "@/components/Skeletonx";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+    <>
+      <Backgroudpage></Backgroudpage>
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Test></Test>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        HEROUI + NEXTJS + DOCKER
-      </footer>
-    </div>
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <Image
+              className="dark:invert"
+              src="/next.svg"
+              alt="Next.js logo"
+              width={110}
+              height={38}
+              priority
+            /> +
+            <Image
+              className="dark:invert"
+              src="/images/heroui.png"
+              alt="Next.js logo"
+              width={50}
+              height={25}
+              priority
+            />
+          </div>
+
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+
+            <Skeletonx></Skeletonx>
+
+          </div>
+
+          <div className="flex gap-4 items-center flex-col sm:flex-row">
+
+
+            HEROUI + NEXTJS + DOCKER
+          </div>
+
+        </main>
+
+      </div>
+    </>
+
   );
 }
---
+++ src/components/Backgroundpage.tsx
@@ -0,0 +1,9 @@
+import "@/css/style.css"
+export default function Backgroudpage() {
+    return (
+        <div className="ocean">
+            <div className="wave"></div>
+            <div className="wave"></div>
+        </div>
+    )
+}
