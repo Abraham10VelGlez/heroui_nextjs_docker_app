ab547a4ab463868da410cca03f0e850de45b71dd
Abraham10VelGlez <usbrifa@gmail.com>
2025-03-10 15:39:31 -0600

    update_dividir_docker_servidor

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   //CONFIGURACIONES PARA TRABAJAR CON DOCKER SOBRE LA APP
-  reactStrictMode: true,
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000, // Ajusta el tiempo de escucha de cambios
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  //solo se aplica en modo desarrollo de docker ...(process.env.NODE_ENV === "development" && {
+  ...(process.env.NODE_ENV === "development" && {
+    reactStrictMode: true,
+    webpackDevMiddleware: (config) => {
+      config.watchOptions = {
+        poll: 1000, // Ajusta el tiempo de escucha de cambios
+        aggregateTimeout: 300,
+      };
+      return config;
+    },
+  }),
 };
 
 export default nextConfig;
