5789149f44886a2b67681f584fe64e9fe7d29afb
Abraham10VelGlez <usbrifa@gmail.com>
2025-03-10 15:02:17 -0600

    app_ajustes_inicio_pantalla_para_cell_monitor

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -1,4 +1,5 @@
 import Backgroudpage from "@/components/Backgroundpage";
+import Drawerx from "@/components/Drawerx";
 import Keyx from "@/components/Login/Keyx";
 
 export default function Page2() {
@@ -8,7 +9,7 @@ export default function Page2() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <div className="flex gap-4 items-center flex-col sm:flex-row">
-            LISTO
+            <Drawerx></Drawerx>
             <Keyx></Keyx>
 
           </div>
